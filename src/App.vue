19e4df0581b57fa94fcd77592b2b5e5cb6272ead
white-kite <bjw0103@naver.com>
2024-06-02 16:36:43 +0900

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+    <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <!-- <router-link to="/about">About</router-link> -->
+    </nav>
+    <router-view/>
+  </div>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
