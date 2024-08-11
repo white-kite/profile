f638a881736690109630814939dcaa20a1239e56
white-kite <bjw0103@naver.com>
2024-08-11 18:37:25 +0900

    브라우저 내장 메서드 사용

--- src/views/ProjectDetail/ChristmasPrj.vue
+++ src/views/ProjectDetail/ChristmasPrj.vue
@@ -1,7 +1,11 @@
 <template>
-  <label>ChristmasPrj 입니다.</label>
+    <label>ChristmasPrj 입니다.</label>
 
-  <v-btn variant="tonal" href="/projects">되돌아가기</v-btn>
+    <v-btn variant="tonal" @click="goBack">되돌아가기</v-btn>
 </template>
-<script setup></script>
+<script setup>
+const goBack = () => {
+    window.history.back(); // 이전 페이지로 이동하는 브라우저의 내장 메서드
+};
+</script>
 <style scoped></style>
